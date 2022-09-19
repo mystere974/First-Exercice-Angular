fa9da4b2fb454cd5e661800536f0cfe035d7d13f
Vynie <vtouzalin33@gmail.com>
2022-09-19 16:33:13 +0200

    changed title inside app component ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myFirstAngularApp';
+  title = 'Welcome on Loghann\s ship playa';
 }
